db96965f2d92430f78688d0b114fc9fc36c40d9e
MarcoAntonioMGDA <35369000+MarcoAntonioMGDA@users.noreply.github.com>
2021-09-18 16:02:33 -0500

    Upload the Project

---
+++ views/pages/about.php
@@ -0,0 +1,41 @@
+<div class="page-section" id="concept">
+   <h1>¿Quiénes Somos?</h1>
+    <p class="fs18px lh2em tac"><em>"Nadie sabe el valor de los momentos hasta que se convierte en recuerdos"</em></p>
+   <br>
+    <div class="flex-s-a flex-vc">
+        <div class="box-5 box-m-10 box-p-12 concept-box">
+            <h2>Misión</h2>
+            <p class="service-about">Llevar inspiración e innovación a cada familia afectada por la pérdida de un ser querido, brindando un servicio único en Latinoamérica para alivianar a un deudo, utilizando herramientas tecnológicas, artísticas y ecológicas.</p>
+        </div>
+        <div class="box-7 box-m-10 box-p-12 concept-box">
+            <h2>Visión</h2>
+            <p class="service-about">Respeto, competitividad basada en la incorporación de nuevas tradiciones, honestidad, orientación profesional y responsabilidad social. Ante el sensible dolor que deja la partida de un ser querido brindamos un servicio único en el país fusionando con las herramientas tecnológicas de tendencia, todo esto, en un ambiente nuevo de respeto y esperanza donde nuestros sensibles clientes reciban una atención de calidad, enfocado a todas las personas que han tenido que enfrentar este dolor.</p>
+        </div>
+    </div>
+</div>
+<div class="page-section">
+    <div class="flex-s-a">
+        <div class="box-6 lh2em p10px box-m-12">
+            <h2>Antecedentes</h2>
+            <p class="fs18px lh2em taj"><strong>ECUAFOREVER</strong> surge de la necesidad de nuestros clientes, de hacer perdurar el recuerdo de sus seres queridos, aprovechando las bondades del entorno tecnológico, artístico y ecológico actual.</p>
+            <div class="w50p mauto">
+                <img src="views/img/pages/home/logo.png" alt="Full Logo" class="dblock mauto w100p">
+            </div>
+        </div>
+       <div class="box-6 lh2em p10px box-m-12">
+            <h2>Nuestros Servicios</h2>
+            <p class="fs18px lh2em taj">Contamos con nuevas metodologías innovadoras para el manejo de los recuerdos de nuestros seres queridos.</p>
+            <p class="fs18px lh2em taj">Inmortalizamos las memorias y vivencias mediante la elaboración de una fotografía de alta calidad impresa en lienzo y en el caso de CREMACIÓN; con una parte de las cenizas del difunto en un entorno cubierto de resina liquida que perdurará para toda la vida con una placa conmemorativa.</p>
+            <p class="fs18px lh2em taj">Una planta de tu preferencia para que con el tiempo se convierta en un frondoso árbol longevo que represente el recuerdo de tu ser querido, o si prefieres un árbol longevo Bonsai para que lo pueda poner en un lugar interno de tu preferencia.</p>
+            <p class="fs18px lh2em taj">La edición, producción, masterización y publicación de un video de honras con fotografías, información y relatos de los seres queridos, este video estará publicado en nuestra plataforma de EcuaForever para que puedan acceder con un código QR de forma permanente el momento que lo deseen.</p>
+        </div>
+        <div class="box-6 lh2em p10px box-m-12">
+            <h2>Nuestro Equipo</h2>
+            <p class="fs18px lh2em taj">Contamos con personal capacitado para todos los servicios ofrecidos, así como; para la elaboración, producción y edición de nuestros trabajos audiovisuales, con artistas calificados en las ramas de música, pintura, orfebrería y más áreas relacionadas.</p>
+        </div>
+        <div class="box-6 lh2em p10px box-m-12">
+            <h2>Nuestra Comunidad</h2>
+            <p class="fs18px lh2em taj">Nuestro objetivo es llegar a todas las personas que necesiten de nuestro servicio que han sido afectadas por esta inevitable experiencia, brindando nuestra profesionalidad en el manejo respetuoso y responsable de tan valiosos recuerdos, toda vez que contamos con una infraestructura tecnológica adecuada, podemos ofrecer nuestro producto a nivel mundial.</p>
+        </div>
+    </div>
+</div>
---
+++ views/pages/services.php
@@ -0,0 +1,123 @@
+<div class="page-section">
+    <h1>Nuestros Servicios</h1>
+    <br>
+    <p class="fs18px lh2em tar"><em><strong>"La vida de los muertos se coloca en la memoria de los vivos."</strong><br>Marcus Tulliuis Cicaro</em></p>
+    <br>
+    <p class="fs18px lh2em taj">“Quien pasó por nuestra vida y dejó luz, resplandecerá en nuestra alma para toda la eternidad”.</p>
+    <p class="fs18px lh2em taj">La pérdida de un ser querido es algo que a todos nos cuesta mucho poder afrontar, tanto si es un amigo como un familiar, los sentimientos que esta pérdida nos provoca, pueden ser realmente muy dolorosos, es una situación donde los sentimientos afloran y debemos ser muy sensibles y respetuosos.</p>
+    <p class="fs18px lh2em taj">Encontrar aquellas maneras que nos permitan ayudar a conllevar el dolor de los deudos es nuestro trabajo.</p>
+    <p class="fs18px lh2em tar"><em>“El significado espiritual de un bonsái es la eternidad”.</em></p>
+    <br>
+    <p class="fs20px lh2em tac">Por todo esto <strong>EcuaForever</strong> presenta los siguientes planes a tu disposición:</p>
+</div>
+<div class="page-section pricing">
+    <h1 class="fs40px tac">Planes</h1>
+    <br>
+    <br>
+    <div class="flex-s-a flex-vc">
+        <div class="box-4 box-i-6 box-m-10 box-p-12 plan" data-aos="zoom-in" data-aos-duration="800">
+            <div class="top">
+                <img src="views/img/pages/pricing/cont1.jpg" alt="">
+            </div>
+            <h2>Deluxe</h2>
+            <!-- <p class="price"><span class="fs20px"><i class="fas fa-dollar-sign"></i></span> 450.00</p> -->
+            <div class="body">
+                <p class="fs14px mb5px"><em><i class="far fa-dot-circle"></i> Retrato de alta calidad en lienzo
+                        (40x50cm), con los datos de su preferencia.</em></p>
+                <p class="fs14px mb5px pl10px"><em><i class="fas fa-dot-circle"></i> (Opcional) se puede colocar parte
+                        de las cenizas como decorativo dentro del cuadro enmarcado</em></p>
+                <p class="fs14px mb5px"><em><i class="far fa-dot-circle"></i> Árbol de la vida a elegir con placa conmemorativa.</em></p>
+                <p class="fs14px mb5px pl10px"><em><i class="fas fa-dot-circle"></i> Longevo o Bonsai.</em></p>
+                <p class="fs14px mb5px"><em><i class="far fa-dot-circle"></i> Vídeo conmemorativo de 3 minutos publicado en nuestra plataforma, acceso con código QR permanente.</em></p>
+                <p class="fs14px"><em><i class="far fa-dot-circle"></i> 2 celebraciones religiosas online del 1er mes y del año.</em></p>
+                <a target="_blank"
+                    href="https://api.whatsapp.com/send?phone=593986381050&text=%C2%A1Hola!%20Me%20gustar%C3%ADa%20contratar%20su%20plan%20*Deluxe*"
+                    class="btn"><i class="fas fa-shopping-basket"></i> Comprar</a>
+            </div>
+        </div>
+        <div class="box-4 box-i-6 box-m-10 box-p-12 plan" data-aos="zoom-in" data-aos-duration="800">
+            <div class="top">
+                <img src="views/img/pages/pricing/cont2.jpg" alt="">
+            </div>
+            <h2>Platinium</h2>
+            <!-- <p class="price"><span class="fs20px"><i class="fas fa-dollar-sign"></i></span> 500.00</p> -->
+            <div class="body">
+                <p class="fs14px mb5px"><em><i class="far fa-dot-circle"></i> Retrato de alta calidad en lienzo
+                        (50x60cm), con los datos de su preferencia.</em></p>
+                <p class="fs14px mb5px pl10px"><em><i class="fas fa-dot-circle"></i> (Opcional) se puede colocar parte
+                        de las cenizas como decorativo dentro del cuadro enmarcado</em></p>
+                <p class="fs14px mb5px pl10px"><em><i class="fas fa-dot-circle"></i> Marco del retrato de aluminio o
+                        latón plateado </em></p>
+                <p class="fs14px mb5px"><em><i class="far fa-dot-circle"></i> Árbol de la vida a elegir con placa
+                        conmemorativa.</em></p>
+                <p class="fs14px mb5px pl10px"><em><i class="fas fa-dot-circle"></i> Longevo o Bonsai.</em></p>
+                <p class="fs14px mb5px"><em><i class="far fa-dot-circle"></i> Vídeo conmemorativo de 3 minutos publicado
+                        en nuestra plataforma, acceso con código QR permanente.</em></p>
+                <p class="fs14px"><em><i class="far fa-dot-circle"></i> 2 celebraciones religiosas online del 1er mes y
+                        del año.</em></p>
+                <a target="_blank"
+                    href="https://api.whatsapp.com/send?phone=593986381050&text=%C2%A1Hola!%20Me%20gustar%C3%ADa%20contratar%20su%20plan%20*Platinium*"
+                    class="btn"><i class="fas fa-shopping-basket"></i> Comprar</a>
+            </div>
+        </div>
+        <div class="box-4 box-i-6 box-m-10 box-p-12 plan" data-aos="zoom-in" data-aos-duration="800">
+            <div class="top">
+                <img src="views/img/pages/pricing/cont3.jpg" alt="">
+            </div>
+            <h2>Diamond</h2>
+            <!-- <p class="price"><span class="fs20px"><i class="fas fa-dollar-sign"></i></span> 650.00</p> -->
+            <div class="body">
+            <p class="fs14px mb5px"><em><i class="far fa-dot-circle"></i> Retrato de alta calidad en lienzo (60x80cm), con los datos de su preferencia.</em></p>
+                <p class="fs14px mb5px pl10px"><em><i class="fas fa-dot-circle"></i> (Opcional) se puede colocar parte de las cenizas como decorativo dentro del cuadro enmarcado</em></p>
+                <p class="fs14px mb5px pl10px"><em><i class="fas fa-dot-circle"></i> Marco de latón dorado</em></p>
+                <p class="fs14px mb5px pl10px"><em><i class="fas fa-dot-circle"></i> Luces led posteriores</em></p>
+                <p class="fs14px mb5px"><em><i class="far fa-dot-circle"></i> Árbol de la vida a elegir con placa conmemorativa.</em></p>
+                <p class="fs14px mb5px pl10px"><em><i class="fas fa-dot-circle"></i> Longevo o Bonsai.</em></p>
+                <p class="fs14px mb5px"><em><i class="far fa-dot-circle"></i> Vídeo conmemorativo de 3 minutos publicado en nuestra plataforma, acceso con código QR permanente.</em></p>
+                <p class="fs14px"><em><i class="far fa-dot-circle"></i> 2 celebraciones religiosas online del 1er mes y del año.</em></p>
+                <a target="_blank" href="https://api.whatsapp.com/send?phone=593986381050&text=%C2%A1Hola!%20Me%20gustar%C3%ADa%20contratar%20su%20plan%20*Diamond*" class="btn"><i class="fas fa-shopping-basket"></i> Comprar</a>
+            </div>
+        </div>
+
+        <div class="box-4 box-i-6 box-m-10 box-p-12 plan" data-aos="zoom-in" data-aos-duration="800">
+            <div class="top">
+                <img src="views/img/pages/pricing/cont4.jpg" alt="">
+            </div>
+            <h2>Future</h2>
+            <!-- <p class="price"><span class="fs20px"><i class="fas fa-dollar-sign"></i></span> 650.00</p> -->
+            <div class="body">
+                <p class="fs14px mb5px"><em><i class="far fa-dot-circle"></i> Accesible en todos los planes.</em></p>
+                <p class="fs14px mb5px"><em><i class="far fa-dot-circle"></i> Tu última voluntad Vídeo de despedida
+                        familiar que será almacenado hasta el momento requerido.</em></p>
+                <a target="_blank"
+                    href="https://api.whatsapp.com/send?phone=593986381050&text=%C2%A1Hola!%20Me%20gustar%C3%ADa%20contratar%20su%20plan%20*Future*"
+                    class="btn"><i class="fas fa-shopping-basket"></i> Comprar</a>
+            </div>
+        </div>
+    </div>
+    <p class="tac lh2em fs15px maxw900px mauto">Los precios descritos en los paquetes <strong>no incluyen impuestos</strong>; además ofrecemos modalidades de PrePago y PostPago.</p>
+</div>
+<div class="page-section">
+    <h1>¿Porque escoger nuestros servicios?</h1>
+    <br>
+    <p class="fs18px lh2em tar"><em><strong>"La muerte lleva consigo un dolor que nadie podrá sanar excepto al mantener vivo un recuerdo que exprese el amor infinito".</strong><br>Porque ayudamos a alivianar la carga del dolor ante la pérdida de un ser querido, por lo que ofrecemos asesoramiento y asistencia, facilitando las labores que conlleva el proceso de despedida con respeto y altivez.</em></p>
+    <br>
+    <p class="fs18px lh2em taj">EcuaForever "La muerte se da solo cuando nos olvidamos".</p>
+    <p class="fs18px lh2em taj">La pérdida de un ser querido es algo que a todos nos cuesta mucho poder afrontar. Tanto si es un amigo como un familiar, los sentimientos que esta pérdida nos provoca, pueden ser realmente muy dolorosos. Es una situación donde los sentimientos afloran y debemos ser muy sensibles y respetuosos.</p>
+    <p class="fs18px lh2em taj">Para más información nos puedes encontrar en nuestros canales de contactos.</p>
+    <p class="fs18px lh2em tar"><em><strong>"La muerte solo existe cuando llega el olvido."</strong><br>EcuaForever</em>
+    </p>
+    <!-- <ul class="pl25px gray">
+        <li class="fs18px lh2em taj">La fusión de una parte de las cenizas en un hermoso cuadro cubierto con resina
+            liquida para que dure toda la vida.</li>
+        <li class="fs18px lh2em taj">La fusión con otra parte de las cenizas con una planta recién florida para que se
+            conviertan en un árbol longevo que represente lo que en vida fue y lo puedan plantar en el lugar que lo
+            deseen o, si desean la opción de reemplazar en vez de la planta le fusionamos con un pequeño árbol bonsái
+            para que lo mantengan en el interior de su casa denominado el "ÁRBOL DE LA VIDA".</li>
+        <li class="fs18px lh2em taj">La creación de un video conmemorativo que será publicado en un espacio PRIVADO de
+            la plataforma EcuaForever para que todos los familiares que tengan un código QR puedan visitar cuando
+            deseen, y disfrutar los recuerdos de nuestro ser querido que vivirá por siempre.</li>
+        <li class="fs18px lh2em taj">Celebramos una misa o culto presencial de acuerdo a su preferencia de manera
+            virtual al primer mes y al primer año de fallecido.</li>
+    </ul> -->
+</div>
